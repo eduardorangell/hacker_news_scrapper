4c466e550911bc222c412a84a8e17be50c3e6e56
Eduardo Rangell <rangellferreira@gmail.com>
2019-01-09 16:12:33 -0200

    Novo teste com outro link

--- lib/hacker_news_scraper.dart
+++ lib/hacker_news_scraper.dart
@@ -6,12 +6,12 @@ import 'package:html/dom.dart';
 
 Future initiate(BaseClient client) async {
   // Make API call to Hackernews homepage
-  Response response = await client.get('https://news.ycombinator.com');
+  Response response = await client.get('https://digitalsummit.com/resources/');
   if (response.statusCode != 200) return response.body;
 
   // Use html parser
   var document = parse(response.body);
-  List<Element> links = document.querySelectorAll('td.title > a.storylink');
+  List<Element> links = document.querySelectorAll('div.isds-post-title > h4 > a');
   List<Map<String, dynamic>> linkMap = [];
 
   for (var link in links) {
